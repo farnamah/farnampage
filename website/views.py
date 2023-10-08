b435ea3d4accf44f69d4bc57c21b81b4e4540090
farnamah <145441216+farnamah@users.noreply.github.com>
2023-10-08 10:17:44 +0330

    Created first app

---
+++ website/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index_view(request):
+    return HttpResponse('home page')
+
+
+def about_view(request):
+    return HttpResponse('about page')
+
+
+def contact_view(request):
+    return HttpResponse('contact page')
