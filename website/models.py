deaa9cc552a69b03b1272f0f4b5e7b818941c0b0
farnamah <145441216+farnamah@users.noreply.github.com>
2023-10-09 17:56:54 +0330

    Models and Templates
    adding models and templates

--- website/models.py
+++ website/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+#class Post(models.Model):
+    #title = models.CharField(max_length=255)
+    #content = models.TextField()
+    #image
+    #category
+    #counted_views = models.IntegerField(default=0)
+    #status = models.BooleanField(default=False)
+    #published_date = models.DateTimeField(null=True)
+    #created_date = models.DateTimeField(auto_now_add=True)
+    #updated_date = models.DateTimeField(auto_now=True)
 
-# Create your models here.
+class Contact(models.Model):
+    name = models.CharField(max_length=225)
+    email = models.EmailField()
+    subject = models.CharField(max_length=225)
+    message = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
--- website/views.py
+++ website/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def index_view(request):
     return render(request, 'website/index.html')
 
 
 def about_view(request):
-    return render(request, 'website/about.html')
+    return render(request, 'website/index.html')
 
 
 def contact_view(request):
-    return render(request, 'website/contact.html')
+    return render(request, 'website/index.html')
