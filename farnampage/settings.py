5db7c6aacde42849b257e4da2dd3e5355f343586
farnamah <145441216+farnamah@users.noreply.github.com>
2023-10-12 18:52:59 +0330

    Fixing setting

--- farnampage/settings.py
+++ farnampage/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -51,7 +51,7 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'farnampage.urls'
-import os
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -116,8 +116,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_URL = '/statics/'
+# 2
 
 
 MEDIA_URL = '/media/'
--- website/admin.py
+++ website/admin.py
@@ -4,9 +4,11 @@ from website.models import Contact
 
 class ContactAdmin(admin.ModelAdmin):
     date_hierarchy = 'created_date'
-    list_display = ('name', 'email', 'created_date')
-    ordering = ['created_date']
-    # search_fields = ['name']
+    list_display = ('name', 'message', 'created_date')
+    #ordering = ['created_date']
+    #search_fields = ('name')
+    class Meta:
+        ordering = ['created_date']
 
 
 admin.site.register(Contact, ContactAdmin)
