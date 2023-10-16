8b84241fcc27759a7b80e542532fe6a661e83a4b
farnamah <145441216+farnamah@users.noreply.github.com>
2023-10-16 13:25:22 +0330

    Making form

--- website/views.py
+++ website/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
 from django.shortcuts import render
 
 def index_view(request):
@@ -12,3 +13,9 @@ def about_view(request):
 
 def contact_view(request):
     return render(request, 'website/index.html')
+
+
+def test_view(request):
+    if request.method == 'post':
+        print('post')
+    return render(request, 'website/test.html',{}
