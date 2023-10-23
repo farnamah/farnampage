3f81ef43a5f18a4e66b04484ede1d04917eec8c0
farnamah <145441216+farnamah@users.noreply.github.com>
2023-10-23 21:35:28 +0330

    Done2

--- farnampage/urls.py
+++ farnampage/urls.py
@@ -19,9 +19,11 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('website.urls')),
+    path("__debug__/", include("debug_toolbar.urls")),
 
 ]
 
