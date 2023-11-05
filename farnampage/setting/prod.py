0a23fa0ac81dd3c52886d8a3c04a0c84758634cd
AmirMahdi Qiasvand <amirmahdiqiasvand@gmail.com>
2023-11-05 16:09:23 +0330

    Update prod.py

--- farnampage/setting/prod.py
+++ farnampage/setting/prod.py
@@ -25,10 +25,10 @@ DATABASES = {
 
 MEDIA_ROOT = BASE_DIR / 'media'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-# STATICFILES_DIRS = [
-#     BASE_DIR / 'static',
-# ]
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 CSRF_COOKIE_SECURE = True
