75c18c6dff142ede4988ce4ecbf6e50eac94baca
AmirMahdi Qiasvand <amirmahdiqiasvand@gmail.com>
2023-11-05 16:46:47 +0330

    fixes
    fixes

--- farnampage/settings.py
+++ farnampage/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2f8(mialduic*t87q%j@08^cq#j*6s+^p#o7j$6)6_id@fimt_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["159.100.30.92"]
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
@@ -114,7 +114,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 MEDIA_URL = '/media/'
 
